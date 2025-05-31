1e5c0efe407e48418463fabd151693d714670852
huycodedao <phunghuyk24@gmail.com>
2025-05-31 10:59:57 +0700

    create-website-layout

---
+++ lib/constants.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "NxtAmzn"
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "Your Amazon Clone"
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "A Next.js Amazon clone with Tailwind CSS, TypeScript, and more."
